62562bc2f8b954e9dddf5c93b89daaac919b2cda
airasz <rirulez.mz@gmail.com>
2024-09-09 11:39:11 +0700

    piradio

---
+++ oledUI.py
@@ -0,0 +1,144 @@
+ 
+import Adafruit_SSD1306
+
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+from time import sleep
+
+# Raspberry Pi pin configuration:
+RST = None     # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+#draw.rectangle((0,0,20,20),outline=0,fill=255)
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+
+# Load default font.
+font = ImageFont.load_default()
+
+
+class oled:
+    def showvolpercent(self, vol):
+            #draw.text((x,(width-8)), vol, font=font, fill=255)
+            draw.rectangle((0,top+20,width,30), outline=0, fill=0)
+            #draw.text((x,top+25), "           ", font=font, fill=255)
+            disp.image(image)
+            sleep(.1)
+            draw.text((x,top+20), vol, font=font, fill=255)
+            disp.image(image)   
+            disp.display()
+            
+            return
+    def wipe(self):
+        draw.rectangle((0,0,width,height), outline=0, fill=0)
+        disp.image(image)   
+        disp.display()
+       # disp.clear()
+
+        return
+    
+    def settext(self, line, string):
+       
+        
+        #draw.rectangle((0,0,width,height), outline=0, fill=0)
+        if line==1:
+            draw.rectangle((0,0,width,10), outline=0, fill=0)
+            draw.text((x,top), string, font=font, fill=255)
+        if line==2:
+            draw.rectangle((0,10,width,20), outline=0, fill=0)
+            draw.text((x,top+10), string, font=font, fill=255)
+        if line==3:
+            draw.rectangle((0,20,width,30), outline=0, fill=0)
+            draw.text((x,top+20), string, font=font, fill=255)
+        if line==4:
+            draw.rectangle((0,32,width,height), outline=0, fill=0)
+            draw.text((x,top+25), string, font=font, fill=255)
+
+        disp.image(image)   
+        disp.display()
+       # disp.clear()
+
+        return
+        
+    def showstation(self , s):
+       # lcd.lcd_display_string("Playing", 1)
+        #lcd.lcd_display_string("                ", 2)
+        #settext(1, "Playing")
+        disp.clear()
+        draw.rectangle((0,0,width,top+30), outline=0, fill=0)
+        #disp.image(image)
+        #disp.display()
+        draw.text((x,top), "Now Playing", font=font, fill=255)
+        draw.text((x,top+10), "          ", font=font, fill=255)
+        #settext(2, "             ") 
+        
+        if s == 1:           
+#            draw.text((x,top), "Radio Rodja LQ", font=font, fill=255)
+#            lcd.lcd_display_string("Radio Rodja LQ", 2)
+            #settext(2, "Radio Rodja LQ")
+             draw.text((x,top+10), "Radio Rodja LQ", font=font, fill=255)
+        if s == 2:
+#            lcd.lcd_display_string("Radio ray FM", 2)
+           # settext(2, "Radio ray FM") 
+             draw.text((x,top+10), "Radio ray FM", font=font, fill=255)
+        if s == 3:
+             draw.text((x,top+10), "Radio muslim", font=font, fill=255)
+        if s == 4:
+             draw.text((x,top+10), "Radio annasihah", font=font, fill=255)
+        if s == 5:
+             draw.text((x,top+10), "Radio al-iman", font=font, fill=255)
+        if s == 6:
+             draw.text((x,top+10), "Idza'atul Khair", font=font, fill=255)
+        if s == 7:
+             draw.text((x,top+10), "Radio Kita Cirebon", font=font, fill=255)
+        if s == 8:
+             draw.text((x,top+10), "Radio Insani FM", font=font, fill=255)
+        if s == 9:
+             draw.text((x,top+10), "Audio rodjaTV", font=font, fill=255)
+        if s == 21:
+             draw.text((x,top+10), "Radio Quran 1", font=font, fill=255)
+        if s == 22:
+             draw.text((x,top+10), "Radio Quran 2", font=font, fill=255)
+        if s == 23:
+             draw.text((x,top+10), "Radio Quran 3", font=font, fill=255)
+        if s == 24:
+             draw.text((x,top+10), "Radio Quran 4", font=font, fill=255)
+
+        # Display image.
+        disp.image(image)
+        disp.display()
+        
+
+        return
---
+++ radioled.py
@@ -0,0 +1,466 @@
+#!usr/bin/python
+# requires RPi_I2C_driver.py
+#import RPi_I2C_driver
+#import lcdui
+import Adafruit_SSD1306
+import oledUI
+import os
+import RPi.GPIO as GPIO
+from time import sleep
+# from apscheduler.scheduler import Scheduler
+# set up the pins
+
+# currentvol = 0
+#blocking = 0;
+muted=False
+#pylirc.init("pylirc",  blocking)
+sleep(1)
+# import subprocess
+GPIO.setmode(GPIO.BCM)
+# GPIO.setup(21,  GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(21, GPIO.IN)
+GPIO.setup(20, GPIO.IN)
+GPIO.setup(16, GPIO.IN)
+GPIO.setup(19, GPIO.IN)
+GPIO.setup(17, GPIO.IN)
+GPIO.setup(4, GPIO.IN)
+
+#mylcd = RPi_I2C_driver.lcd()
+myoled= oledUI.oled()
+#ui=lcdui.show(()
+#ui=oledUI.show()
+# scheduler
+# schd=Scheduler()
+
+# test 2
+#mylcd.lcd_display_string("RPi ", 1)
+#mylcd.lcd_display_string("Internet Radio", 2)
+myoled.settext(1, "raspberry pi")
+myoled.settext(2, "Internet Radio")
+myoled.settext(3, "by airasz")
+os.system("amixer cset numid=3 2")      # 2 is force to HDMI OUTPUT   1 analog
+sleep(.5)
+
+#  vol = open('piRadioSavedVol', 'r')
+#  currentvol = int(vol.read())
+#  vol.close()
+#  mylcd.lcd_display_string_pos("V:", 1, 12)
+#  mylcd.lcd_display_string_pos(str(currentvol), 1, 14)
+
+def saveVol():
+    global currentvol
+    vol = open('vol', 'w')
+    vol.write(str(volpercent))
+    vol.close()
+
+
+def loadVol():
+    global currentvol
+    vol = open('vol', 'r')
+    currentvol = int(vol.read())
+    volpercent = currentvol
+    #mylcd.lcd_display_string_pos("V:", 1, 12)
+    #mylcd.lcd_display_string_pos(str(currentvol), 1, 14)
+    vol.close()
+
+loadVol()
+
+sleep(.5)  # 2 sec delay
+
+# mylcd.lcd_clear()
+
+angka = 0
+
+
+def counting():
+    # import time
+    global angka
+    while True:
+        angka += 1
+        sleep(0.5)
+        if angka > 60:
+            #mylcd.backlight(0)
+            angka = 0
+
+# counting()
+
+
+volpercent = currentvol
+# volpercent = 70
+volstring = ""
+volstring1 = "amixer sset  PCM,0 "
+volstring2 = "%"
+volume = ""
+vol_dir = False
+
+
+def setvolumeUP():
+    global volpercent
+    #global station
+    #myoled.showstation(station)
+    volpercent += 2
+    if volpercent > 100:
+        volpercent = 100
+    volstring = str(volpercent)
+    volume = (volstring1 + volstring + volstring2)
+    os.system(volume)
+    print volume
+    #mylcd.lcd_display_string_pos("V:", 1, 12)
+    #mylcd.lcd_display_string_pos(volstring, 1, 14)
+    #ui.showvolbar(volpercent)
+    volumestr="volume : "
+    myoled.showvolpercent(volumestr +volstring)
+
+def  mute(mute):
+    global volpercent
+    global tmp_vol
+    if mute==True:
+        tmp_vol=volpercent
+        volpercent=50
+        volstring = str(volpercent)
+        volume = (volstring1 + volstring + volstring2)
+        os.system(volume)
+        myoled.settext(3, "Muted")
+    else:
+        #loadVol()
+        sleep(.2)
+        volpercent=tmp_vol
+        volstring = str(volpercent)
+        volume = (volstring1 + volstring + volstring2)
+        volumestr="volume : "
+        os.system(volume)
+        myoled.showvolpercent(volumestr +volstring)
+        
+def setvolumeDOWN():
+    global volpercent
+    #global station
+    #myoled.showstation(station)
+    volpercent -= 2
+    if volpercent < 0:
+        volpercent = 0
+    volstring = str(volpercent)
+    volume = (volstring1 + volstring + volstring2)
+    os.system(volume)
+    print volume
+    #mylcd.lcd_display_string_pos("V:", 1, 12)
+    #mylcd.lcd_display_string_pos(volstring, 1, 14)
+    #ui.showvolbar(volpercent)    
+    volumestr="volume : "
+    myoled.showvolpercent(volumestr +volstring)
+
+
+# os.system("piradio")
+# os.system("amixer sset  PCM,0 70%")
+volstring = str(volpercent)
+os.system(volstring1 + volstring + volstring2)
+print (volstring1 + volstring + volstring2)
+station = 0
+volume = 9
+count_s_mode = 1
+radio_mode = 1
+
+
+def pickstationUP():
+    global station
+    global rpwr
+    global radio_mode
+    #  if station == 0:
+            # loadVol()
+    if station == 9:
+        os.system("vlc vlc://quit")
+        sleep(1)
+    station += 1
+    rpwr = True
+    # print station
+    #ui.showstation(station)
+    if station == 1:
+        # global volpercent
+        # loadVol()
+        os.system("piradio rodjaLOW")
+        print "play rodja LQ"
+
+    if station == 3:
+        os.system("piradio muslim")
+        print "play muslim"
+
+    if station == 8:
+        os.system("piradio insani")
+        print "play Insani"
+
+    if station == 4:
+        #os.system("amixer sset  PCM,0 74%")
+        os.system("piradio annashihah1")
+        print "play annasihah1"
+
+    if station == 5:
+        os.system("piradio aliman")
+        print "play aliman"
+
+    if station == 42:
+        os.system("piradio annashihah2")
+        print "play annasihah2"
+       # mylcd.lcd_clear()
+        #mylcd.lcd_display_string("Playing", 1)
+        #mylcd.lcd_display_string("Radio annashihah2", 2)
+
+    if station == 6:
+        os.system("piradio idza")
+        print "play idza'atul khair"
+
+    if station == 88:
+        os.system("piradio rodjaLOW")
+        print "play rodja LOW"
+
+    if station == 2:
+        #os.system("amixer sset  PCM,0 74%")
+        os.system("piradio rayfm")
+        print "play ray FM"
+
+    if station == 7:
+        os.system("piradio cirebon")
+        print "rdio kita crebon"
+
+    if station == 19:
+        os.system("piradio cirebon")
+        print "rdio kita crebon"
+
+    if station == 9:
+        os.system("vlc --intf dummy http://audio.rodja.tv:1010/;stream.mp3")
+        print "audio rodjaTV"
+
+    if station == 21:
+        os.system("piradio radioquran")
+        print "radio qur'an 1"
+
+    if station == 22:
+        os.system("piradio qcoran")
+        print "radio qur'an 2"
+
+    if station == 23:
+        os.system("piradio quranradio")
+        print "radio qur'an 3"
+
+    if station == 24:
+        os.system("piradio coranweb")
+        print "radio qur'an 4"
+
+    if (radio_mode == 1) and (station > 9):
+        station = 1
+
+    if (radio_mode == 2) and (station > 24):
+        station = 21
+        
+    myoled.showstation(station)
+
+    return
+
+
+def pickstationDOWN():
+    global station
+    global rpwr
+    if station == 9:
+        os.system("vlc vlc://quit")
+        sleep(1)
+    station -= 1
+    rpwr = True
+    # print station
+    
+    #ui.showstation(station)
+    if station == 1:
+        os.system("piradio rodjaLOW")
+        print "play rodja"
+
+    if station == 3:
+        os.system("piradio muslim")
+        print "play muslim"
+
+    if station == 8:
+        os.system("piradio insani")
+        print "play Insani"
+
+    if station == 4:
+        #os.system("amixer sset  PCM,0 74%")
+        os.system("piradio annashihah1")
+        print "play annasihah1"
+
+    if station == 5:
+        os.system("piradio aliman")
+        print "play aliman"
+
+    if station == 55:
+        os.system("piradio annashihah2")
+        print "play annasihah2"
+        #mylcd.lcd_clear()
+        #mylcd.lcd_display_string("Playing", 1)
+        #mylcd.lcd_display_string("Radio annashihah2", 2)
+
+    if station == 6:
+        os.system("piradio idza")
+        print "play idza'atul khair"
+
+    if station == 17:
+        os.system("piradio rodjaLOW")
+        print "play rodja LOW"
+
+    if station == 2:
+        #os.system("amixer sset  PCM,0 74%")
+        os.system("piradio rayfm")
+        print "play ray FM"
+
+    if station == 7:
+        os.system("piradio cirebon")
+        print "rdio kita crebon"
+
+    if station == 9:
+        os.system("vlc --intf dummy http://audio.rodja.tv:1010/;stream.mp3")
+        print "audio rodjatv"
+
+    if station == 21:
+        os.system("piradio radioquran")
+        print "radio qur'an 1"
+
+    if station == 22:
+        os.system("piradio qcoran")
+        print "radio qur'an 2"
+
+    if station == 23:
+        os.system("piradio quranradio")
+        print "radio qur'an 3"
+
+    if station == 24:
+        os.system("piradio coranweb")
+        print "radio qur'an 4"
+
+    if (radio_mode == 1) and (station < 1):
+        station = 9
+
+    if (radio_mode == 2) and (station < 21):
+        station = 24
+    
+    myoled.showstation(station)
+
+    return
+
+
+rpwr = True
+
+
+def power():
+    global rpwr
+    global station
+    if rpwr is False:
+        rpwr = True
+        os.system("piradio on")
+        print "piradio on"
+        myoled.showstation(station)
+    else:
+        rpwr = False
+        os.system("sudo killall -9 mpd")
+        print "piradio off"
+        myoled.settext(1, "Raspberry pi")
+        myoled.settext(2, "Internet Radio")
+        myoled.settext(3, "Stopped")
+
+def ir_listener():
+        s=lirc.nextcode()
+        if s!=[]:
+            print s
+            if s[0]=="KEY_RIGHT":
+                if station > 0:
+                    os.system("sudo killall -9 mpd")
+                    print "key up remote pressed"
+                sleep(0.2)
+                pickstationUP()
+            if s[0]=="KEY_LEFT":
+                if station > 0:
+                    os.system("sudo killall -9 mpd")
+                    print "key up remote pressed"
+                sleep(0.2)
+                pickstationDOWN()
+            if s[0]=="KEY_UP":
+                setvolumeUP()
+                saveVol()
+                sleep(0.2)
+            if s[0]=="KEY_DOWN":  
+                setvolumeDOWN()
+                saveVol()
+                sleep(0.2)
+            if s[0]=="KEY_POWER":
+                myoled.settext(1,"restarting device")
+                sleep(1)                      
+                os.system("sudo reboot")
+                sleep(0.2)
+            if s[0]=="KEY_MUTE":                      
+                if muted==False:
+                    muted=True
+                    mute(True)
+                else:
+                    muted=False
+                    mute(False)
+                sleep(0.2)
+            if s[0]=="KEY_102ND":
+                if station > 0:
+                    # os.system("piradio off")
+                    os.system("sudo killall -9 mpd")
+                if radio_mode == 1:
+                    radio_mode = 2
+                    station = 21
+                else:
+                    radio_mode = 1
+                    station = 1
+                pickstationUP()
+                sleep(0.1)
+                if s[0]=="KEY_PLAY":
+                    power()
+                    sleep(.2)
+
+
+try:
+    while True:
+        if GPIO.input(21):
+            angka = 0
+            if station > 0:
+                if station!=9:
+                    os.system("sudo killall -9 mpd")
+                    sleep(0.1)
+            pickstationDOWN()
+
+        if GPIO.input(20):
+            if station > 0:
+                if station!=9:
+                    os.system("sudo killall -9 mpd")
+                    sleep(0.1)
+            pickstationUP()
+        if GPIO.input(17):
+            angka = 0
+            power()
+            sleep(0.2)
+        if GPIO.input(19):
+            angka = 0
+            print "GPIO 19 HIGH"
+            setvolumeDOWN()
+            saveVol()
+            sleep(0.2)
+        if GPIO.input(16):
+            angka = 0
+            print "GPIO 16 HIGH"
+            setvolumeUP()
+            saveVol()
+            sleep(0.2)
+        if GPIO.input(4):
+            angka = 0
+            print "GPIO 4 HIGH"
+            if station > 0:
+                os.system("sudo killall -9 mpd")
+            if radio_mode == 1:
+                radio_mode = 2
+                station = 21
+            else:
+                radio_mode = 1
+                station = 1
+            pickstationUP()
+            sleep(0.1)
+        else:
+            sleep(0.2)
+finally:
+    GPIO.cleanup()
