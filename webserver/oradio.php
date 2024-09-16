1b1970f0e36e3710c6559bfa91597cf80f695d5a
airasz <rirulez.mz@gmail.com>
2024-09-17 00:55:39 +0700

    sasd

--- webserver/oradio.php
+++ webserver/oradio.php
@@ -9,7 +9,19 @@ if (isset($_GET["cmd"])) {
     $dt = $_GET["cmd"];
     if ($dt === "status") {
         $status =shell_exec("mpc");
-        echo $status;
+        $newlinepos = strpos($status , "\n"); // find line break in status
+        $volumepos = strpos($status, "volume");
+
+
+        if ($volumepos == 0) {
+            echo "paused";
+        } else {
+            $stnname = substr($status, 0, $newlinepos); // get station name
+            echo $stnname; // display station name
+        }
+        // echo $status;
+        echo PHP_EOL;
+    echo substr($status, $volumepos+7, 4); // display current volume
     }  else {
         
         $status = shell_exec($dt);
