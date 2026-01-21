aaa4480b279cb61a8ed108c0139df733912db350
airasz <rirulez.mz@gmail.com>
2026-01-21 12:13:30 +0700

    fix text wrap

--- python/bananapi/module_piradionex.py
+++ python/bananapi/module_piradionex.py
@@ -196,6 +196,8 @@ class sleeptimer:
             nextion_page = 4
             MyNextion.send_command("page 4")
             MyNextion.send_command("dim=30")
+            MyNextion.send_command("t1.isbr=1")  # sukses 1=true 0=false
+            MyNextion.send_command("t1.xcen=Center")
 
     # get auto stop second running
     def getsecac(self):
